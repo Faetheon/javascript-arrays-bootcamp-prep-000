80d764aa06df2fc848eef147aac7a23f45c8e3b9
Nathan Hill <nateohill@gmail.com>
2018-05-18 21:10:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   element = [element];
-  const Array = [array, ...element];
+  const Array = [...element, array];
   return Array;
 }
